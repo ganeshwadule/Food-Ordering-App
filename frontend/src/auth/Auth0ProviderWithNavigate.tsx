ba6814bd3cf6faff89982530e45ea99af63976ff
09xxUni <uniquegbw09@gmail.com>
2024-03-24 16:42:53 +0530

    User Request

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserAPI";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const { createUser} = useCreateMyUser();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -14,9 +16,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
        throw new Error("unable to initialise auth");
     }
   const onRedirectCallback = (appState?:AppState,user?:User)=>{
-    console.log("AppState: ",appState)
-    console.log("User: ", user);
-  }
+    if(user?.sub && user?.email){
+      createUser({auth0Id : user?.sub, email : user?.email})
+    }
+  } 
   return(
     <Auth0Provider
       domain={domain}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,27 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 
+// React Query is a library for React that simplifies data fetching, caching, and state management for server-side data. It provides hooks for fetching data, caching it, handling loading and error states, and managing mutations like creating, updating, and deleting data. It aims to make handling server state in React applications easier and more efficient.
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries:{
+        refetchOnWindowFocus : false,
+    },
+  },
+})
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <QueryClientProvider client={queryClient}>
       <Auth0ProviderWithNavigate>
       <AppRoutes/>
       </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
